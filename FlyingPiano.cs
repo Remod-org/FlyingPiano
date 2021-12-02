9799da9b58e7dab6ec36a2fcbde7a10a718f9079
Remod <desolationoutpostpve@gmail.com>
2021-12-02 15:03:40 -0600

    Fix for December 2021 update

--- FlyingPiano.cs
+++ FlyingPiano.cs
@@ -1,20 +1,25 @@
-#region License (MIT)
+#region License (GPL v3)
 /*
-Copyright RFC1920 <desolationoutpostpve@gmail.com>
+    DESCRIPTION
+    Copyright (c) RFC1920 <desolationoutpostpve@gmail.com>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
-is furnished to do so, subject to the following conditions:
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
 */
-#endregion License Information (MIT)
+#endregion License Information (GPL v3)
 using Oxide.Core;
 using System;
 using System.Collections;
@@ -29,7 +34,7 @@ using Newtonsoft.Json;
 
 namespace Oxide.Plugins
 {
-    [Info("FlyingPiano", "RFC1920", "1.0.3")]
+    [Info("FlyingPiano", "RFC1920", "1.0.4")]
     [Description("Fly a piano!")]
     // Thanks to Colon Blow for his fine work on GyroCopter, upon which this was originally based.
     internal class FlyingPiano : RustPlugin
@@ -560,7 +565,7 @@ namespace Oxide.Plugins
             UnityEngine.Object.Destroy(newPiano.GetComponent<DestroyOnGroundMissing>());
             UnityEngine.Object.Destroy(newPiano.GetComponent<GroundWatch>());
             BaseMountable chairmount = newPiano.GetComponent<BaseMountable>();
-            chairmount.needsVehicleTick = true;
+            chairmount.isMobile = true;
             newPiano.enableSaving = false;
             newPiano.OwnerID = player.userID;
             newPiano.Spawn();
@@ -1029,7 +1034,7 @@ namespace Oxide.Plugins
                 BaseMountable hasmount = entity.GetComponent<BaseMountable>();
                 if (hasmount != null)
                 {
-                    hasmount.needsVehicleTick = true;
+                    hasmount.isMobile = true;
                 }
             }
 
